5ad1f819e4cda50dc3bcec56df52d4191a160424
Jess Rezac <jess.rezac@gmail.com>
2019-06-06 18:08:30 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,5 +1,5 @@
 module Dance
-  def twirling
+  def twirl
     "I'm twirling!"
   end
 
@@ -14,5 +14,5 @@ module Dance
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
-  
+
 end
